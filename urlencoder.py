438d13ee1b322e8adda17d34dd6e49f1fd33d642
Mario González <78421275+m4riio21@users.noreply.github.com>
2023-02-17 14:33:59 +0100

    Update parameter handling and help panel

--- urlencoder.py
+++ urlencoder.py
@@ -6,21 +6,25 @@ def ascii2hex(source):
     return hex(ord(source))
 
 def main(state):
-	result = ""
-	strin = sys.argv[1]
-	if state != "":
-		strin = state
+        result = ""
+        strin = sys.argv[1]
+        if state != "":
+                strin = state
 
-	for char in strin:
-		result += ascii2hex(char)
+        for char in strin:
+                result += ascii2hex(char)
 
-	return result.replace("0x", "%")
+        return result.replace("0x", "%")
 
 if __name__ == "__main__":
-	res = main("")
+        if len(sys.argv) <= 1:
+                print("Usage: python3", sys.argv[0], "<path_to_encode>")
+                sys.exit(1)
+        res = main("")
 
-	if "--double" in sys.argv:
-		print(main(res))
-		sys.exit(0)
+        if "--double" in sys.argv:
+                print(main(res))
+                sys.exit(0)
 
-	print(res)
+        print(res)
+
